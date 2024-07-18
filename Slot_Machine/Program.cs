d4af3ab7f0543bea016f0e30a3b6a45aae2ae7d0
Sosa09 <soufiane.arrazouki@hotmail.com>
2024-07-18 21:03:45 +0200

    Added user experience output (console.writeline) on how much he can bet and reading his bet

--- Slot_Machine/Program.cs
+++ Slot_Machine/Program.cs
@@ -8,6 +8,7 @@ namespace Slot_Machine
         {
             //instantiating Random class to get random numbers for the slot machine
             Random random = new Random();
+           
             const int START_MONEY = 30; //virtual money every gamer starts with 
             int playerMoney = START_MONEY; //assigning the start money right away, it will hold the total money after the game
             int profite = 0; //loses or winnings of the users
@@ -18,10 +19,14 @@ namespace Slot_Machine
             {
                 //ALONG THE GAME THE SYSTEM WILL CHECK IF USER HAS ENOUGH MONEY TO PLAY
                 while (!isPlayerMoneyZero) 
-                { 
+                {
                     //BET MIN 1$ max 3$
+                    Console.WriteLine("Please bet min 1 or max 3 dollars to spin");
+
+                    int.TryParse(Console.ReadLine(), out int playerBet); //reading end user input and parsing it to an int
 
                     //BASED ON  BET DEFINE ARRAYS
+
                 
                 }
             }
