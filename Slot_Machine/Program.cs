4b0dc12bb509f26d4a7b24e09f0ddd2b8291bc56
Sosa09 <soufiane.arrazouki@hotmail.com>
2024-07-18 20:55:50 +0200

    declared the base variables random, START_MONEY, playerMoney, profite
    see comments in program for more details

--- Slot_Machine/Program.cs
+++ Slot_Machine/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Slot_Machine
+﻿using System.Data.SqlTypes;
+
+namespace Slot_Machine
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //instantiating Random class to get random numbers for the slot machine
+            Random random = new Random();
+            const int START_MONEY = 30; //virtual money every gamer starts with 
+            int playerMoney = START_MONEY; //assigning the start money right away, it will hold the total money after the game
+            int profite = 0; //loses or winnings of the users
+
+            
+
+         
         }
     }
 }
