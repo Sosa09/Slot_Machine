63ab619486616f4ad1ba08248cf96d45c817ff19
Sosa09 <soufiane.arrazouki@hotmail.com>
2024-07-19 20:10:14 +0200

    added if statement on userinput int.TryParse(Console.ReadLine(), out int playerBet) line 43 to give better userexperience
    second: add GRID_ROW and GRID_COL to define the grid

--- Slot_Machine/Program.cs
+++ Slot_Machine/Program.cs
@@ -17,11 +17,17 @@ namespace Slot_Machine
             const int FIRST_POSITION_OF_DIMENSIONAL_ARRAY = 0;
             const int SECOND_POSITION_OF_DIMENSIONAL_ARRAY = 0;
             const int START_MONEY = 30; //virtual money every gamer starts with 
+
             const int MINIMUM_BET = 1;
             const int MAXIMUM_BET = 3;
+
+            const int GRID_ROW = 3;
+            const int GRID_COL = 3;
+
             int playerMoney = START_MONEY; //assigning the start money right away, it will hold the total money after the game
             int profite = 0; //loses or winnings of the users
-            int dollarPerRow = 1;
+
+
             bool winningRow = false;
             bool isPlayerMoneyNotZero = true; //return true if user has no money to player anymore and will end the game
 
@@ -34,62 +40,68 @@ namespace Slot_Machine
                     //BET MIN 1$ max 3$
                     Console.WriteLine($"Please bet minimum {MINIMUM_BET} or max {MAXIMUM_BET} dollars to spin");
 
-                    int.TryParse(Console.ReadLine(), out int playerBet); //reading end user input and parsing it to an int
-
-                    //BASED ON  BET DEFINE ARRAYS AND STORE RANDOM VALUES, player bet defines the first dimensional array
-
-                    int[,] slot = new int[playerBet, MAX_SPIN_VALUE];
-
-                    //looping trough the array.
-                    //going throuhg the first dimension and then the second where we store the value to the second dimensional array for later check and print the value to the end user
-        
-
-                    for (int i = 0; i < slot.GetLength(FIRST_POSITION_OF_DIMENSIONAL_ARRAY); i++)
+                    if (int.TryParse(Console.ReadLine(), out int playerBet))
                     {
-                        
-                        for (int j = 0; j < slot.GetLength(SECOND_POSITION_OF_DIMENSIONAL_ARRAY); j++)
+
+                        int[,] slot = new int[GRID_ROW, GRID_COL];
+
+                        //looping trough the array.
+                        //going throuhg the first dimension and then the second where we store the value to the second dimensional array for later check and print the value to the end user
+
+
+                        for (int i = 0; i < slot.GetLength(FIRST_POSITION_OF_DIMENSIONAL_ARRAY); i++)
                         {
-                            int randomNr = random.Next(MAX_SPIN_VALUE);
-                            slot[i, j] = randomNr;
-                            Console.Write($"{randomNr} ");
+
+                            for (int j = 0; j < slot.GetLength(SECOND_POSITION_OF_DIMENSIONAL_ARRAY); j++)
+                            {
+                                int randomNr = random.Next(MAX_SPIN_VALUE);
+                                slot[i, j] = randomNr;
+                                Console.Write($"{randomNr} ");
+
+                            }
+
+                            Console.WriteLine();
 
                         }
-                        
-                        Console.WriteLine();
-                    }
 
 
-                    //lloping to see if there is a winning row REPITITIVE CODE BAD and MAGICNUMBERS
-                    for (int i = 0; i < slot.GetLength(FIRST_POSITION_OF_DIMENSIONAL_ARRAY); i++)
-                    {
-                        if (slot[i, 0] == slot[i, 1] && slot[i, 0] == slot[i, 2])
-                            winningRow = true;
+                        //lloping to see if there is a winning row REPITITIVE CODE BAD and MAGICNUMBERS
+                        for (int i = 0; i < slot.GetLength(FIRST_POSITION_OF_DIMENSIONAL_ARRAY); i++)
+                        {
+                            if (slot[i, 0] == slot[i, 1] && slot[i, 0] == slot[i, 2])
+                                winningRow = true;
 
-                    }
+                        }
 
-                    if (winningRow)
-                    {
-                        //add 1 dollar to profilte and to pllayer monay
-                        
-                        profite++;
-                        playerMoney++;
+                        if (winningRow)
+                        {
+                            //add 1 dollar to profilte and to pllayer monay
 
-                        Console.WriteLine($"you won, your actual profit is {profite} and total money is {playerMoney}");
+                            profite++;
+                            playerMoney++;
+
+                            Console.WriteLine($"you won, your actual profit is {profite} and total money is {playerMoney}");
+                        }
+                        else
+                        {
+                            playerMoney--;
+                            profite--;
+                            Console.WriteLine($"you lost, your actual profit is {profite} and total money is {playerMoney}");
+
+                        }
+
+
                     }
                     else
                     {
-                        playerMoney--;
-                        profite--;
-                        Console.WriteLine($"you lost, your actual profit is {profite} and total money is {playerMoney}");
-
+                        Console.WriteLine($"{playerBet} is not a valid number, please try again!");
                     }
+
+                  
 
 
                 }
-
                 Console.WriteLine($"you quitted the game with {profite}. see you");
-
-
             }
         }
     }
